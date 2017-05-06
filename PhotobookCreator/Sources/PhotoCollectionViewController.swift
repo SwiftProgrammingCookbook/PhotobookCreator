94f207d6200db89c5eeeace22e795d4754dfe8a4
Keith Moon <keef@keefmoon.com>
2017-05-06 18:09:08 +0100

    Adding cropping function

--- PhotobookCreator/Sources/PhotoCollectionViewController.swift
+++ PhotobookCreator/Sources/PhotoCollectionViewController.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 import QuartzCore
+import Dispatch
 
 class PhotoCollectionViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
+    var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
     
     var photos = [UIImage]()
-    
-    let builder = PhotoBookBuilder()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,26 +27,52 @@ class PhotoCollectionViewController: UIViewController {
         guard let photoPaths = try? fileManager.contentsOfDirectory(atPath: samplePhotosFolderPath) else { return }
         
         photos = photoPaths.flatMap { UIImage(contentsOfFile: samplePhotosFolderPath.appending($0)) }
+        
+        // Setup Activity Indicator
+        activityIndicator.hidesWhenStopped = true
+        let barButtonItem = UIBarButtonItem(customView: activityIndicator)
+        navigationItem.leftBarButtonItem = barButtonItem
     }
     
     @IBAction func generateButtonPressed(sender: UIBarButtonItem) {
-        generatePhotoBook()
+        
+        activityIndicator.startAnimating()
+        
+        generatePhotoBook(with: photos) { [activityIndicator] photobookURL in
+            
+            activityIndicator.stopAnimating()
+            
+            let previewController = UIDocumentInteractionController(url: photobookURL)
+            previewController.delegate = self
+            previewController.presentPreview(animated: true)
+        }
     }
     
+    let processingQueue = DispatchQueue(label: "Photo processing queue")
     
-    func generatePhotoBook() {
+    func generatePhotoBook(with photos: [UIImage], completion: @escaping (URL) -> Void) {
         
-        let resizedPhotos = resizeAndCropToSmallest(of: photos)
-        
-        let now = Date()
-        let photobookURL = builder.buildPhotobook(with: resizedPhotos)
-        
-        let previewController = UIDocumentInteractionController(url: photobookURL)
-        previewController.delegate = self
-        previewController.presentPreview(animated: true)
-        
-        let duration = now.timeIntervalSinceNow
-        print("Duration: \(duration)")
+        processingQueue.async {
+            
+            let now = Date()
+            
+            let resizer = PhotoResizer()
+            let builder = PhotoBookBuilder()
+            
+            // Scale down (can take a while)
+            var photosForBook = resizer.scaleToSmallest(of: photos)
+            // Crop (can take a while)
+            photosForBook = resizer.cropToSmallest(of: photosForBook)
+            // Generate PDF (can take a while)
+            let photobookURL = builder.buildPhotobook(with: photosForBook)
+            
+            let duration = now.timeIntervalSinceNow
+            print("Duration: \(duration)")
+            
+            DispatchQueue.main.async {
+                completion(photobookURL)
+            }
+        }
     }
 }
 
@@ -61,16 +87,14 @@ extension PhotoCollectionViewController: UITableViewDataSource {
         let photo = photos[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath)
         cell.imageView?.image = photo
-        cell.textLabel?.text = "\(photo.size.width) x \(photo.size.height)"
+        cell.textLabel?.text = "\(Int(photo.size.width)) x \(Int(photo.size.height))"
         return cell
     }
-    
 }
 
 extension PhotoCollectionViewController: UIDocumentInteractionControllerDelegate {
     
     func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
-        
         return navigationController!
     }
 }
--- PhotobookCreator/Sources/PhotoResizer.swift
+++ PhotobookCreator/Sources/PhotoResizer.swift
@@ -9,61 +9,73 @@
 import UIKit
 import CoreImage
 
-func resizeAndCropToSmallest(of photos: [UIImage]) -> [UIImage] {
-    
-    var smallestHeight = CGFloat.greatestFiniteMagnitude
-    var smallestWidth = CGFloat.greatestFiniteMagnitude
-    
-    photos.forEach { photo in
-        smallestHeight = min(smallestHeight, photo.size.height)
-        smallestWidth = min(smallestWidth, photo.size.width)
-    }
+class PhotoResizer {
     
     let context = CIContext(options: [kCIContextUseSoftwareRenderer: false])
     
-    let scaledImages: [UIImage] = photos.flatMap { photo in
+    func scaleToSmallest(of photos: [UIImage]) -> [UIImage] {
         
-        guard let cgImage = photo.cgImage else { return nil }
+        var smallestHeight = CGFloat.greatestFiniteMagnitude
+        var smallestWidth = CGFloat.greatestFiniteMagnitude
         
-        // Calculate scale needed to fit the closest smallest dimension
-        let heightScale = smallestHeight / photo.size.height
-        let weightScale = smallestWidth / photo.size.width
-        let heightScaleDifference = abs(1 - heightScale)
-        let weightScaleDifference = abs(1 - weightScale)
+        photos.forEach { photo in
+            smallestHeight = min(smallestHeight, photo.size.height)
+            smallestWidth = min(smallestWidth, photo.size.width)
+        }
         
-        let closestScale = heightScaleDifference < weightScaleDifference ? heightScale: weightScale
+        let scaledImages: [UIImage] = photos.flatMap { photo in
+            
+            guard let cgImage = photo.cgImage else { return nil }
+            
+            // Calculate scale needed to fit the closest smallest dimension
+            let heightScale = smallestHeight / photo.size.height
+            let weightScale = smallestWidth / photo.size.width
+            let heightScaleDifference = abs(1 - heightScale)
+            let weightScaleDifference = abs(1 - weightScale)
+            
+            let closestScale = heightScaleDifference < weightScaleDifference ? heightScale: weightScale
+            let image = CIImage(cgImage: cgImage)
+            
+            let filter = CIFilter(name: "CILanczosScaleTransform")!
+            filter.setValue(image, forKey: "inputImage")
+            filter.setValue(closestScale, forKey: "inputScale")
+            filter.setValue(1.0, forKey: "inputAspectRatio")
+            
+            guard let outputImage = filter.value(forKey: "outputImage") as? CIImage else { return nil }
+            guard let scaledCGImage = context.createCGImage(outputImage, from: outputImage.extent) else { return nil }
+            
+            let scaledUIImage = UIImage(cgImage: scaledCGImage)
+            return scaledUIImage
+        }
         
-        print("--------------------------")
-        print("Image Size: \(photo.size)")
-        print("Height Scale: \(heightScale)")
-        print("Weight Scale: \(weightScale)")
-        print("Height Scale Diff: \(heightScaleDifference)")
-        print("Weight Scale Diff: \(weightScaleDifference)")
-        print("Closest Scale: \(closestScale)")
-        print("+++++++++++++++++++++")
-        
-        let image = CIImage(cgImage: cgImage)
-        
-        print("CIImage: \(image)")
-        
-        let filter = CIFilter(name: "CILanczosScaleTransform")!
-        filter.setValue(image, forKey: "inputImage")
-        filter.setValue(closestScale, forKey: "inputScale")
-        filter.setValue(1.0, forKey: "inputAspectRatio")
-        guard let outputImage = filter.value(forKey: "outputImage") as? CIImage else { return nil }
-        
-        print("Scaled CIImage: \(outputImage)")
-        
-        guard let scaledCGImage = context.createCGImage(outputImage, from: outputImage.extent) else { return nil }
-        
-        print("Scaled CGImage: \(scaledCGImage)")
-        
-        let scaledUIImage = UIImage(cgImage: scaledCGImage)
-        
-        print("Scaled UIImage: \(scaledUIImage)")
-        
-        return scaledUIImage
+        return scaledImages
     }
     
-    return scaledImages
+    func cropToSmallest(of photos: [UIImage]) -> [UIImage] {
+        
+        var smallestHeight = CGFloat.greatestFiniteMagnitude
+        var smallestWidth = CGFloat.greatestFiniteMagnitude
+        
+        photos.forEach { photo in
+            smallestHeight = min(smallestHeight, photo.size.height)
+            smallestWidth = min(smallestWidth, photo.size.width)
+        }
+        
+        let croppedImages: [UIImage] = photos.flatMap { photo in
+            
+            guard let cgImage = photo.cgImage else { return nil }
+            
+            // Calculate center crop rect
+            let x = (photo.size.width - smallestWidth) / 2
+            let y = (photo.size.height - smallestHeight) / 2
+            let croppingRect = CGRect(x: x, y: y, width: smallestWidth, height: smallestHeight)
+            
+            let croppedImage = CIImage(cgImage: cgImage).cropping(to: croppingRect)
+            
+            guard let scaledCGImage = context.createCGImage(croppedImage, from: croppedImage.extent) else { return nil }
+            let scaledUIImage = UIImage(cgImage: scaledCGImage)
+            return scaledUIImage
+        }
+        return croppedImages
+    }
 }
